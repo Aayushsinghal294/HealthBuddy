686c5f12fff8d6293c97c766acb0d83c78b3c76f
Aayushsinghal294 <23bcs003@iiitdmj.ac.in>
2025-04-13 15:24:04 +0530

    css adjustments

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,11 +3,16 @@ export default{
     theme: {
         extend:{
         colors:{
-            'primary':"#111111",
+            primary: '#0f766e', // teal-700
+            secondary: '#e0f2f1', // teal-50
+            accent: '#14b8a6', // teal-500
         },
         gridTemplateColumns: {
             'auto': 'repeat(auto-fill, minmax(200px, 1fr))',
         },
+        fontFamily: {
+            sans: ['Inter', 'sans-serif'],
+          },
     },
 },
     plugins: [tailwindcss(),react()],
